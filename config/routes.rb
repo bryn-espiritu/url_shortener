52f74e6654872a0e7475bde305c0466cd960ca26
brynex20 <brynespiritu@gmail.com>
2022-11-25 01:23:17 +0800

    move devise notice to new
    move devise notice to new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :links
   devise_for :users
+  root "posts#new"
+  get '/:code', to: 'links#show'
+  post '/links', to: 'links#create'
+
+  resources :posts, only: :show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get '/s/:slug', to: 'links#show', as: :short
-  get '/s/:slug', to: 'links#slug'
+  # get '/s/:slug', to: 'links#slug'
   # Defines the root path route ("/")
   # root "articles#index"
 end
