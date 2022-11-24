5c8bd7261f0c9f46ac9a0eadfc759d6244360912
brynex20 <brynespiritu@gmail.com>
2022-11-24 21:53:28 +0800

    add resources to routes & get '/s/:slug', to: 'links#slug'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :links
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # get '/s/:slug', to: 'links#show', as: :short
+  get '/s/:slug', to: 'links#slug'
   # Defines the root path route ("/")
   # root "articles#index"
 end
