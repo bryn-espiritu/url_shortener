36065806265f8ed14740f4f7795f36461339a237
brynex20 <brynespiritu@gmail.com>
2022-11-24 21:58:20 +0800

    destroy controller model link

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_102333) do
-  create_table "links", charset: "utf8mb4", force: :cascade do |t|
-    t.string "url"
-    t.string "slug"
-    t.string "shorten"
-    t.integer "clicked", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_030933) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
