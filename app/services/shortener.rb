2b5d18d6510d35c6afc943e31dae6ec9453e3f5a
brynex20 <brynespiritu@gmail.com>
2022-11-25 01:26:09 +0800

    Fix Shortener Service (Forgot to add to recreation of Link Controller

---
+++ app/services/shortener.rb
@@ -0,0 +1,25 @@
+class Shortener
+  attr_reader :url, :link_model
+
+  def initialize(url, link_model = Link)
+    @url = url
+    @link_model = link_model
+  end
+
+  def generate_short_link
+    link_model.create(long_url: url, code: code)
+  end
+
+  def code
+    loop do
+      code = get_fresh_code
+      break code unless link_model.exists?(code: code)
+    end
+  end
+
+  private
+
+  def get_fresh_code
+    SecureRandom.uuid[0..6]
+  end
+end
